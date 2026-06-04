50f8895234fba67e5422afc8360ffd88f9081c85
TAITAI0804 <taiyaki0141z@icloud.com>
2026-06-04 07:02:20 +0000

    wotk11-4

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 let todos=[];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDo App' });
+  res.render('index', { title: 'ToDo App' ,todos: todos,});
 });
 
 router.post('/',function(req,res,next){
   const todo=req.body.add;
   todos.push(todo);
-  res.render('index', { title: 'ToDo App' });
+  res.redirect('/');
 });
 
 module.exports = router;
