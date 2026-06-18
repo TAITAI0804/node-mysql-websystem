63af64d4c72ab9d618159aa08e1fdb90c177b8b7
TAITAI0804 <taiyaki0141z@icloud.com>
2026-06-18 06:38:04 +0000

    komit1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+});
 
 let todos=[];
 /* GET home page. */
@@ -8,6 +16,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/',function(req,res,next){
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo=req.body.add;
   todos.push(todo);
   res.redirect('/');
