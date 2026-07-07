1bcf3351744e1c0e85b4e8419b6a0968f90354ea
TAITAI0804 <taiyaki0141z@icloud.com>
2026-07-07 09:00:45 +0000

    komit17-3

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
@@ -23,16 +24,22 @@ router.post('/', function (req, res, next) {
       password: password,
     })
     .select("*")
-    .then((results) => {
+    .then(async function (results) {
       if (results.length === 0) {
         res.render("signin", {
           title: "Sign in",
           errorMessage: ["ユーザが見つかりません"],
           isAuth: isAuth,
         });
-      } else {
+      } else if (await bcrypt.compare(password, results[0].password)) {
         req.session.userid = results[0].id;
         res.redirect('/');
+      } else {
+        res.render("signin", {
+          title: "Sign in",
+          errorMessage: ["ユーザが見つかりません"],
+          isAuth: isAuth,
+        });
       }
     })
     .catch(function (err) {
