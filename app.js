f337df4d1cab178e751f5549ab1fb8f89f69413e
TAITAI0804 <taiyaki0141z@icloud.com>
2026-07-09 06:04:41 +0000

    komit18-1

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// authorization
+require("./config/passport")(app);
+
 // router
-app.use("/", require("./routes"));
+app.use('/', require('./routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
